e01d9372c6cb05d14fd93988483ca6b0c53838c5
ghaschel <ghaschel@gmail.com>
2014-10-29 11:13:10 -0200

    1.0.3
    The JS functions now are organized inside a object. For better code
    handling.

--- dist/js/treasure.js
+++ dist/js/treasure.js
@@ -8,116 +8,122 @@
                                                                   
 
  */
+var treasure = {
+	detectBrowser: {
+		ie: function() { //detecta se o navegador atual é o IE
+			var ua = window.navigator.userAgent;
+			var msie = ua.indexOf('MSIE ');
+			var trident = ua.indexOf('Trident/');
 
-jQuery.fn.scrollBottom = function() { 
-  return $(document).height() - this.scrollTop() - this.height(); 
-}; //Oposto de scrollTop
+			if (msie > 0) {
+				return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+			}
 
-jQuery.expr[':'].parents = function(a,i,m){
-    return jQuery(a).parents(m[3]).length < 1;
-}; //seletor para descelecionar um elemento cujo elemento pai tenha a classe determinada nos parenteses. Funciona com filter, e direto no seletor @Paolo Bergantino - http://stackoverflow.com/questions/965816/what-jquery-selector-excludes-items-with-a-parent-that-matches-a-given-selector
+			if (trident > 0) {
+				var rv = ua.indexOf('rv:');
+				return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+			}
 
-function detectIE() {
-    var ua = window.navigator.userAgent;
-    var msie = ua.indexOf('MSIE ');
-    var trident = ua.indexOf('Trident/');
+			return false;
+		}, 
 
-    if (msie > 0) {
-    	return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
-    }
+		ff: function() { //detecta se o navegador atual é o firefox - testado em windows e mac
+			var ua = window.navigator.userAgent;
+			var firefox = ua.indexOf('Firefox ');
+			var gecko = ua.indexOf('Gecko/');
 
-	if (trident > 0) {
-	    var rv = ua.indexOf('rv:');
-	    return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
-	}
+			if (firefox > 0) {
+				return parseInt(ua.substring(firefox + 5, ua.indexOf('.', firefox)), 10);
+			}
 
-	return false;
-} //detecta se o navegador atual é o IE
+			if (gecko > 0) {
+				var rv = ua.indexOf('rv:');
+				return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+			}
 
-function detectFF() {
-    var ua = window.navigator.userAgent;
-    var firefox = ua.indexOf('Firefox ');
-    var gecko = ua.indexOf('Gecko/');
+			return false;
+		},
+		op: function() { //detecta se o navegador atual é o opera - testado em windows e mac
+			var ua = window.navigator.userAgent;
+			var opr = ua.indexOf('OPR/');
 
-    if (firefox > 0) {
-    	return parseInt(ua.substring(firefox + 5, ua.indexOf('.', firefox)), 10);
-    }
+			if (opr > 0) {
+				return parseInt(ua.substring(opr + 4, ua.indexOf('.', opr)), 10);
+			}
 
-	if (gecko > 0) {
-	    var rv = ua.indexOf('rv:');
-	    return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
-	}
+			return false;
+		},
 
-	return false;
-} //detecta se o navegador atual é o firefox - testado em windows e mac
+		sf: function() { //detecta se o navegador atual é o safari - testado em windows
+			var ua = window.navigator.userAgent;
+			var safari = ua.indexOf('Safari/');
+			var version = ua.indexOf('Version/');
 
-function detectOP() {
-    var ua = window.navigator.userAgent;
-    var opr = ua.indexOf('OPR/');
+			if (safari > 0 && version > 0) {
+			    return parseInt(ua.substring(version + 8, ua.indexOf('.', version)), 10);
+			}
 
-	if (opr > 0) {
-	    return parseInt(ua.substring(opr + 4, ua.indexOf('.', opr)), 10);
-	}
+			return false;
+		},
 
-	return false;
-} //detecta se o navegador atual é o opera - testado em windows e mac
+		cr: function() {  //detecta se o navegador atual é o chrome - testado em windows
+			var ua = window.navigator.userAgent;
+			var safari = ua.indexOf('Safari/');
+			var chrome = ua.indexOf('Chrome/');
 
-function detectSF() {
-    var ua = window.navigator.userAgent;
-    var safari = ua.indexOf('Safari/');
-    var version = ua.indexOf('Version/');
+			if (safari > 0 && chrome > 0) {
+				return parseInt(ua.substring(chrome + 7, ua.indexOf('.', chrome)), 10);
+			}
 
-	if (safari > 0 && version > 0) {
-	    return parseInt(ua.substring(version + 8, ua.indexOf('.', version)), 10);
-	}
-
-	return false;
-} //detecta se o navegador atual é o safari - testado em windows
-
-function detectCR() {
-	var ua = window.navigator.userAgent;
-    var safari = ua.indexOf('Safari/');
-    var chrome = ua.indexOf('Chrome/');
-
-	if (safari > 0 && chrome > 0) {
-	    return parseInt(ua.substring(chrome + 7, ua.indexOf('.', chrome)), 10);
-	}
-
-	return false;
-} //detecta se o navegador atual é o chrome - testado em windows
-
-function isEmail(email) {
-	var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-	return regex.test(email);
-} //testa se a string inserida é um email
-
-function between(minimum, value, maximum) {
-	return value > minimum && value < maximum;
-} //thanks to undefined (http://stackoverflow.com/questions/14718561/check-if-a-number-is-between-two-values)
-
-function generateID() { 
-	d = new Date();
-	return date = "id-" + d.getDate() + "-" + d.getMonth() + "-" + d.getFullYear() + "-" + d.getHours() + "-" + d.getMinutes() + "-" + d.getSeconds()  + "-" + d.getMilliseconds();
-} //gera um id único
-
-function magicCall(fnName) {
-	fn = window[fnName];
-	fnExists = typeof fn === 'function';
-	if(fnExists) {
-		fn();
-	}
-} //chama função sem retornar erro de função inexistente caso a mesma não esteja declarada
-
-Array.prototype.clean = function(deleteValue) {
-	for (var i = 0; i < this.length; i++) {
-    	if (this[i] == deleteValue) {         
-			this.splice(i, 1);
-			i--;
+			return false;
 		}
-	}
-	return this;
-} //remove campos em branco da array - @CMS/@kapa - http://stackoverflow.com/questions/281264/remove-empty-elements-from-an-array-in-javascript
+	},
+	extend: {
+		jquery: function() {
+			jQuery.fn.scrollBottom = function() { 
+			  return $(document).height() - this.scrollTop() - this.height(); 
+			}; //Oposto de scrollTop
+			jQuery.expr[':'].parents = function(a,i,m){
+			    return jQuery(a).parents(m[3]).length < 1;
+			}; //seletor para descelecionar um elemento cujo elemento pai tenha a classe determinada nos parenteses. Funciona com filter, e direto no seletor @Paolo Bergantino - http://stackoverflow.com/questions/965816/what-jquery-selector-excludes-items-with-a-parent-that-matches-a-given-selector
+		},
+		js: function() {
+			Array.prototype.clean = function(deleteValue) {
+				for (var i = 0; i < this.length; i++) {
+			    	if (this[i] == deleteValue) {         
+						this.splice(i, 1);
+						i--;
+					}
+				}
+				return this;
+			} //remove campos em branco da array - @CMS/@kapa - http://stackoverflow.com/questions/281264/remove-empty-elements-from-an-array-in-javascript
+		}
+	},
+	tools: {
+		isEmail: function(email) {
+			var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+			return regex.test(email);
+		}, //testa se a string inserida é um email
 
-function getURLParameter(name) {
-  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null
-} // busca o valor de um parametro que foi passado pela url em uma variavel get @David - http://stackoverflow.com/questions/11582512/how-to-get-url-parameters-with-javascript
+		between: function(minimum, value, maximum) {
+			return value > minimum && value < maximum;
+		}, //thanks to undefined (http://stackoverflow.com/questions/14718561/check-if-a-number-is-between-two-values)
+
+		generateID: function() { 
+			d = new Date();
+			return date = "id-" + d.getDate() + "-" + d.getMonth() + "-" + d.getFullYear() + "-" + d.getHours() + "-" + d.getMinutes() + "-" + d.getSeconds()  + "-" + d.getMilliseconds();
+		}, //gera um id único
+
+		magicCall: function(fnName) {
+			fn = window[fnName];
+			fnExists = typeof fn === 'function';
+			if(fnExists) {
+				fn();
+			}
+		}, //chama função sem retornar erro de função inexistente caso a mesma não esteja declarada
+
+		getURLParameter: function(name) {
+		  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null
+		} // busca o valor de um parametro que foi passado pela url em uma variavel get @David - http://stackoverflow.com/questions/11582512/how-to-get-url-parameters-with-javascript
+	}
+};
