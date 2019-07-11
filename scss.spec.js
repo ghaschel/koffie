dd034d0b8f668be00aea13860cdb071040ce41a2
ghaschel <ghaschel@gmail.com>
2019-07-11 00:39:50 -0300

    build(project structure): improved project structure
    added some rules and packages, added sass unit testing, stated a refactor and code clean up
    
    BREAKING CHANGE: initial code clean up and refactor in order for a more maintable code

---
+++ scss.spec.js
@@ -0,0 +1,9 @@
+const path = require('path')
+const sassTrue = require('sass-true')
+const glob = require('glob')
+
+describe('Sass', () => {
+  const sassTestFiles = glob.sync(path.resolve(process.cwd(), 'src/**/*.spec.scss'))
+
+  sassTestFiles.forEach(file => sassTrue.runSass({ file }, { describe, it }))
+})
